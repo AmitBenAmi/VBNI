b3eb1d5bc84b6dec81ad57892b0f814fd2cbe259
ItayGabbay <itay240594@gmail.com>
2018-01-03 13:43:15 +0200

    adding angular and routing

---
+++ VBNI/public/javascripts/app.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+var vbni = angular.module('vbni', ['ngRoute']);
+
+vbni.config(function ($routeProvider, $locationProvider) {
+    $routeProvider.when('/MyGroup', {
+        templateUrl: 'views/mygroup.html',
+        controller: 'GroupCtrl'
+    }).otherwise({
+        templateUrl: 'views/home.html',
+        contollerL: 'MainCtrl'
+    })
+});
+
