336a372f019323c764aa9be0aa7279bec25a3661
ItayGabbay <itay240594@gmail.com>
2018-01-03 15:26:27 +0200

    adding my references

--- VBNI/public/javascripts/MyRefsCtrl.js
+++ VBNI/public/javascripts/MyRefsCtrl.js
@@ -1,4 +1,19 @@
 ﻿angular.module('vbni').controller('MyRefsCtrl', ['$scope', '$http',
     function ($scope, $http) {
 
+        // Getting My references
+        $http.get('/referrals').then(function (res) {
+            data = res.data.map(function (obj) {
+                return {
+                    referrer: obj.referrer,
+                    referenceTo: obj.referenceTo,
+                    clientName: obj.clientName,
+                    isGood: obj.isGood,
+                    amount: obj.amount
+                }
+            });
+            $scope.myRefs = data;
+        }, function (err) {
+            console.log(err);
+        });
     }]);
