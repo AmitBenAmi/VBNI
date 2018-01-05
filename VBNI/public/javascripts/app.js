3a67e45120560ea1f787e637c7817c1c05c6c27d
matanper <matanper@gmail.com>
2018-01-05 18:13:19 +0200

    fix auto close of drawer

--- VBNI/public/javascripts/app.js
+++ VBNI/public/javascripts/app.js
@@ -29,25 +29,41 @@ vbni.config(function ($routeProvider, $locationProvider) {
         .hashPrefix('');
 });
 
+// Adding go back support
 vbni.run(function ($rootScope, $location) {
     
-        var history = [];
-    
-        $rootScope.$on('$routeChangeSuccess', function() {
-            let path = $location.$$path;
-            let pathIndex = history.indexOf(path)
+    var history = [];
 
-            // If path exists, remove it
-            if (pathIndex > -1) {
-                history.splice(pathIndex, 1);
-            }
-            
-            history.push(path);
-        });
-    
-        $rootScope.goBack = function () {
-            var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
-            $location.path(prevUrl);
-        };
-    
+    $rootScope.$on('$routeChangeSuccess', function() {
+        let path = $location.$$path;
+        let pathIndex = history.indexOf(path)
+
+        // If path exists, remove it
+        if (pathIndex > -1) {
+            history.splice(pathIndex, 1);
+        }
+        
+        history.push(path);
     });
+
+    $rootScope.goBack = function () {
+        var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
+        $location.path(prevUrl);
+    };
+    
+});
+
+// Fix problem where in small screens the drawer isn't closing on link click
+vbni.directive('menuClose', function() {
+    return {
+        restrict: 'AC',
+        link: function($scope, $element) {
+            $element.bind('click', function() {
+                var drawer = $('.mdl-layout');
+                if(drawer) {
+                    drawer[0].MaterialLayout.toggleDrawer()
+                }
+            });
+        }
+    };
+});
