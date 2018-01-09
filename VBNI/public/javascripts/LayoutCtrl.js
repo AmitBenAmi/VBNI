4348a8d01191b253195c8d0a7944fe9985e4165a
matanper <matanper@gmail.com>
2018-01-09 21:46:57 +0200

    moved add reference code to layout controller

--- VBNI/public/javascripts/LayoutCtrl.js
+++ VBNI/public/javascripts/LayoutCtrl.js
@@ -37,6 +37,61 @@ function($scope, $http, $rootScope, $location, apiService) {
                $location.path() == '/' ;
     }
 
+    apiService.getMembers().then((members) => {
+        // Filter out the current user
+        $rootScope.members = members.filter(m => m.userName != $rootScope.user.userName);
+    });
+
+    let disposeScopeVars = () => {
+        delete $rootScope.createReferenceClientName;
+        delete $rootScope.referenceReferenceToName;
+        delete $rootScope.createReferenceReferenceTo;
+    };
+
+    let closeDialog = () => {
+        let dialog = $('#createReferenceDialog')[0];
+
+        disposeScopeVars();
+
+        if (dialog) {
+            dialog.close();
+        }
+    };
+    
+    let showCreateReferenceDialog = () => {
+        let dialog = $('#createReferenceDialog')[0];
+
+        if (dialog) {
+            dialog.showModal();
+
+            if (!mdlComponentUpgraded && 
+                getmdlSelect &&
+                typeof(getmdlSelect.init) === 'function') {
+                // Updating Material Design Lite elements (For Dialog of members that value can be shown)
+                getmdlSelect.init(`.${mdlSelectClass}`);
+
+                mdlComponentUpgraded = !mdlComponentUpgraded;
+
+                dialog.addEventListener('keydown', (e) => {
+                    if (e.keyCode === escKey) {
+                        disposeScopeVars();
+                    }
+                });
+            }
+        }
+    };
+
+    let createReference = () => {
+        apiService.createReferral($rootScope.createReferenceReferenceTo.userName, $rootScope.createReferenceClientName)
+            .then(() => {
+                closeDialog();
+            });
+    };
+
+    $rootScope.closeDialog = closeDialog;
+    $rootScope.showCreateReferenceDialog = showCreateReferenceDialog;
+    $rootScope.createReference = createReference;
+    let mdlComponentUpgraded = false;
     setHomepageDetails();
 });
  
--- VBNI/public/javascripts/app.js
+++ VBNI/public/javascripts/app.js
@@ -37,7 +37,6 @@ vbni.config(function ($routeProvider, $locationProvider) {
 vbni.run(['$rootScope', '$location', 'apiService', ($rootScope, $location, apiService) => {
     
     var history = [];
-    let mdlComponentUpgraded = false;
 
     $rootScope.$on('$routeChangeSuccess', function() {
         let path = $location.$$path;
@@ -58,62 +57,9 @@ vbni.run(['$rootScope', '$location', 'apiService', ($rootScope, $location, apiSe
         var prevUrl = history.length > 1 ? history.splice(-2)[0] : "/";
         $location.path(prevUrl);
     };
-
-    apiService.getMembers().then((members) => {
-        // Filter out the current user
-        $rootScope.members = members.filter(m => m.userName != $rootScope.user.userName);
-    });
-
-    let disposeScopeVars = () => {
-        delete $rootScope.createReferenceClientName;
-        delete $rootScope.referenceReferenceToName;
-        delete $rootScope.createReferenceReferenceTo;
-    };
-
-    let closeDialog = () => {
-        let dialog = $('#createReferenceDialog')[0];
-
-        disposeScopeVars();
-
-        if (dialog) {
-            dialog.close();
-        }
-    };
-    
-    let showCreateReferenceDialog = () => {
-        let dialog = $('#createReferenceDialog')[0];
-
-        if (dialog) {
-            dialog.showModal();
-
-            if (!mdlComponentUpgraded && 
-                getmdlSelect &&
-                typeof(getmdlSelect.init) === 'function') {
-                // Updating Material Design Lite elements (For Dialog of members that value can be shown)
-                getmdlSelect.init(`.${mdlSelectClass}`);
-
-                mdlComponentUpgraded = !mdlComponentUpgraded;
-
-                dialog.addEventListener('keydown', (e) => {
-                    if (e.keyCode === escKey) {
-                        disposeScopeVars();
-                    }
-                });
-            }
-        }
-    };
-
-    let createReference = () => {
-        apiService.createReferral($rootScope.createReferenceReferenceTo.userName, $rootScope.createReferenceClientName)
-            .then(() => {
-                closeDialog();
-            });
-    };
-
-    $rootScope.closeDialog = closeDialog;
-    $rootScope.showCreateReferenceDialog = showCreateReferenceDialog;
-    $rootScope.createReference = createReference;
 }]);
+
+vbni.controller('layoutCtrl')
 
 // Fix problem where in small screens the drawer isn't closing on link click
 vbni.directive('menuClose', function() {
