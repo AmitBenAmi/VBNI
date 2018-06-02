8233f5e430b283179cea351609d24df6b37c426e
AmitBenAmi <benamiamit0@gmail.com>
2018-06-03 02:40:51 +0300

    Trying to fix registration

--- VBNI/public/javascripts/RegisterCtrl.js
+++ VBNI/public/javascripts/RegisterCtrl.js
@@ -1,12 +1,31 @@
 'use strict';
 
+angular.module('vbni').directive('onFinishRender', ($timeout) => {
+    return {
+        restrict: 'A',
+        link: (scope, element, attr) => {
+            if (scope.$last === true) {
+                $timeout(() => {
+                    scope.$emit('ngModelFinished');
+                });
+            }
+        }
+    };
+});
+
 angular.module('vbni').controller('RegisterCtrl', ['$scope', 'apiService', '$timeout',
     function ($scope, apiService, $timeout) {
         $scope.$on('$viewContentLoaded', function(event) {
             $timeout(function() {
-                componentHandler.upgradeDom();
-            })
+                componentHandler.upgradeAllRegistered();
+                let registerInputs = document.querySelectorAll('.mdl-js-textfield');
+                for (let i = 0; i < registerInputs.length; i++) {
+                    registerInputs[i].MaterialTextfield.checkDirty();
+                    registerInputs[i].MaterialTextfield.updateClasses_();
+                }
+            });
         });
+
         let onSuccessLogin = (googleUser) => {
             let profile = googleUser.getBasicProfile();
             $scope.user = {
@@ -16,6 +35,14 @@ angular.module('vbni').controller('RegisterCtrl', ['$scope', 'apiService', '$tim
                 password: 'Google',
                 groupId: $scope.$root.user.groupId
             };
+
+            $timeout(function(){
+                let registerInputs = document.querySelectorAll('.mdl-js-textfield');
+                for (let i = 0; i < registerInputs.length; i++) {
+                    registerInputs[i].MaterialTextfield.checkDirty();
+                    registerInputs[i].MaterialTextfield.updateClasses_();
+                }
+            });
         };
 
         let register = (user) => {
